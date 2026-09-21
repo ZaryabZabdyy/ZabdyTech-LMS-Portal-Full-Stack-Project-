9dbb11140711298085994f0c1cbd4986307c5451
Ahmad Zaryab <zarysbahmad@gmail.com>
2026-09-21 09:15:21 +0500

    Updatedr

--- wwwroot/config.js
+++ wwwroot/config.js
@@ -1,4 +1,2 @@
 // config.js
-const API_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
-    ? 'http://localhost:5127'
-    : 'https://zabdytech-lms-portal-full-stack-project-production.up.railway.app'; 
+const API_BASE_URL = 'https://zabdytech-lms-portal-full-stack-project-production.up.railway.app';
